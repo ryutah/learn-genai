2d73d867d1324c548c26f590c4d1ea2e8bbe2edd
ryutah <6662577+ryutah@users.noreply.github.com>
2025-10-05 17:45:08 +0900

    chore: adds playwright e2e test sample

--- add-example/src/app/examples/page.tsx
+++ add-example/src/app/examples/page.tsx
@@ -1,5 +1,9 @@
+import type { Metadata } from "next";
+import generateMetadata from "@/utils/metadata";
 import Example from "./Example";
 import { getSampleData } from "./service";
+
+export const metadata: Metadata = generateMetadata("Example");
 
 export default async function Page() {
 	const posts = await getSampleData();
