5a52b69e526c9e867c50c7079a189bce98bd89b7
ryutah <6662577+ryutah@users.noreply.github.com>
2025-10-19 22:45:03 +0900

    refactor: modify function name

--- add-example/src/app/examples/page.tsx
+++ add-example/src/app/examples/page.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import generateMetadata from "@/utils/metadata";
 import Example from "./Example";
-import { getSampleData } from "./service";
+import { getPosts } from "./service";
 
 export const metadata: Metadata = generateMetadata("Example");
 
 export default async function Page() {
-	const posts = await getSampleData();
+	const posts = await getPosts();
 
 	return <Example response={posts} />;
 }
--- add-example/src/app/examples/service.ts
+++ add-example/src/app/examples/service.ts
@@ -1,6 +1,6 @@
 import type { Post } from "@/types/post";
 import { get } from "@/utils/request";
 
-export async function getSampleData() {
+export async function getPosts() {
 	return await get<Post[]>("https://jsonplaceholder.typicode.com/posts");
 }
